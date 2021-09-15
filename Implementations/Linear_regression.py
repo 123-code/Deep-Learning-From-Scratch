0f031642f15b44dc30a06995b65c7871849b2ea1
123-code <naranjojose256@gmail.com>
2021-09-15 17:00:14 -0500

    First commit

---
+++ Implementations/Linear_regression.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class LinearRegression:
+    def __init__(self,lr=0.001,n_iters = 1000):
+        self.lr = lr
+        self.n_iters = n_iters
+        self.weights = None
+        self.bias = None
+
+    # training function
+
+    def fit(self,X,y):
+        n_samples,n_features = X.shape
+
+        #initializing weights randomly
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+
+        
+
+
+    # predicting function
+
+    def predict(self,X,y):
+        pass
+
+
+
