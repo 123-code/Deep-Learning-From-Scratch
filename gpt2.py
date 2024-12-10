12952f7d4a3a688893e47689b5a6fd737719300e
Jose Ignacio Naranjo <52728126+123-code@users.noreply.github.com>
2024-12-09 22:17:09 -0500

    Update gpt2.py

--- gpt2.py
+++ gpt2.py
@@ -1,3 +1,5 @@
+
+
 from dataclasses import dataclass
 import time 
 import torch
@@ -215,8 +217,28 @@ model = GPT(GPTConfig(vocab_size=50304))
 
 model.to(device)
 model = torch.compile(model)
-optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4,betas=(0.9,0.95)eps=1e-8)
-for i in range(50):
+
+max_lr = 3e-4
+min_lr = max_lr * 0.1
+warmup_steps = 10
+max_steps = 50
+
+def get_lr(it):
+    if it < warmup_steps:
+        return max_lr * (it+1)/warmup_steps
+    if it > max_steps:
+        return min_lr
+    
+    decay_ratio = (it-warmup_steps)/(max_steps - warmup_steps)
+    assert 0 <= decay_ratio <= 1
+
+    coeff = 0.5 * (1.0 + math.cos(math.pi * decay_ratio))
+    return min_lr + coeff * (max_lr - min_lr)
+
+
+
+optimizer = torch.optim.AdamW(model.parameters(),lr=3e-4,betas=(0.9,0.95),eps=1e-8)
+for step in range(max_steps):
     t0 = time.time()
     x,y = train_loader.next_batch()
     x,y = x.to(device),y.to(device)
@@ -225,13 +247,17 @@ for i in range(50):
         logits,loss = model(x,y)
     loss.backward()
     norm = torch.nn.utils.clip_grad_norm_(model.parameters(),1.0)
+    lr = get_lr(step)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
     optimizer.step()
     torch.cuda.synchronize()
     t1 = time.time()
-    dt = (t1-t0)*1000
-    tokens_per_sec = (train_loader.B * train_loader.T)/(t1-t0)
-    print("tokens per second",tokens_per_sec)
-    print(f"step:{i},loss:{loss.item()}")
+    dt = t1-t0
+    tokens_processed = train_loader.B * train_loader.T
+    tokens_per_sec = tokens_processed/dt
+    print("tokens per second: ",tokens_per_sec)
+    print(f"step:{step:4d},loss:{loss.item()}")
 
 sys.exit(0)
 
